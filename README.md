ab5e7642cebdfac710f4ed709366836e22a93e5a
thomasvn <thomasnguyen96@gmail.com>
2023-10-21 13:01:09 -0700

    Update model to 16k context window. Parse args.

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 
 <!-- 
 IDEAS
-- RAG (retrieval augmented API)
-  - Pull contents of all my blog posts and all my Github repos. Ask about what I've accomplished
-  - Pull contents of all Kubecost codebases & docs
+- RAG (retrieval augmented API). Pull contents of all my blog posts. Make it a chat interface.
+  - https://github.com/tmc/langchaingo/blob/main/examples/document-qa-example/document_qa.go
+  - https://github.com/tmc/langchaingo/blob/main/examples/chroma-vectorstore-example/chroma_vectorstore_example.go
+  - Graceful failure when cloning the repo
+  - Need to make the question paramaterized
+- Pull contents of all Kubecost codebases & docs
 - Accessible via API
+-->
+
+<!-- 
+// API returned unexpected status code: 400: This model's maximum context length
+// is 4097 tokens. However, your messages resulted in 11030 tokens. Please
+// reduce the length of the messages.
+
 -->
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -13,84 +12,82 @@ import (
 	"github.com/tmc/langchaingo/schema"
 )
 
-func main() {
-	// if err := run(); err != nil {
-	// 	fmt.Fprintln(os.Stderr, err)
-	// 	os.Exit(1)
-	// }
+const (
+	REPO_URL = "https://github.com/thomasvn/content"
+	DATA_DIR = "./data"
+)
 
-	downloadDocuments()
-	docs, err := loadDocuments()
-	if err != nil {
-		fmt.Println(err)
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
-	fmt.Printf("%+v\n", docs)
 }
 
 func run() error {
+	question := parseArgs()
+	fmt.Println("Question:", question)
+
 	llm, err := openai.New()
 	if err != nil {
 		return err
 	}
 
-	// We can use LoadStuffQA to create a chain that takes input documents and a question,
-	// stuffs all the documents into the prompt of the llm and returns an answer to the
-	// question. It is suitable for a small number of documents.
-	stuffQAChain := chains.LoadStuffQA(llm)
-	docs := []schema.Document{
-		{PageContent: "Harrison went to Harvard."},
-		{PageContent: "Ankush went to Princeton."},
+	err = downloadDocuments()
+	if err != nil {
+		fmt.Println(err)
+	}
+	docs, err := loadDocuments()
+	if err != nil {
+		fmt.Println(err)
 	}
 
+	// Stuffs all documents into the prompt of the llm, and returns an answer to
+	// the question. Suitable for a small number of documents.
+	stuffQAChain := chains.LoadStuffQA(llm)
 	answer, err := chains.Call(context.Background(), stuffQAChain, map[string]any{
 		"input_documents": docs,
-		"question":        "Where did Harrison go to collage?",
+		"question":        question,
 	})
 	if err != nil {
 		return err
 	}
 	fmt.Println(answer)
-
-	// Another option is to use the refine documents chain for question answering. This
-	// chain iterates over the input documents one by one, updating an intermediate answer
-	// with each iteration. It uses the previous version of the answer and the next document
-	// as context. The downside of this type of chain is that it uses multiple llm calls that
-	// cant be done in parallel.
-	refineQAChain := chains.LoadRefineQA(llm)
-	answer, err = chains.Call(context.Background(), refineQAChain, map[string]any{
-		"input_documents": docs,
-		"question":        "Where did Ankush go to collage?",
-	})
-	fmt.Println(answer)
-
 	return nil
 }
 
-func downloadDocuments() {
-	repoURL := "https://github.com/thomasvn/content"
-	destination := "./data/"
-	cmd := exec.Command("git", "clone", repoURL, destination)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		fmt.Printf("Failed to clone repository: %v, output: %s", err, output)
+func parseArgs() string {
+	args := os.Args
+
+	if len(args) != 2 {
+		fmt.Println("Usage: go run main.go <question>")
+		os.Exit(1)
 	}
+
+	return args[1]
 }
 
+// downloadDocuments clones the REPO_URL to the DATA_DIR.
+func downloadDocuments() error {
+	cmd := exec.Command("git", "clone", REPO_URL, DATA_DIR)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to clone repository: %v, output: %s", err, output)
+	}
+	return nil
+}
+
+// loadDocuments walks through the DATA_DIR and loads all Markdown files into
+// the `Document` struct.
 func loadDocuments() ([]schema.Document, error) {
 	var documents []schema.Document
 
-	// Path to the directory
-	dirPath := "./data"
-
-	// Walk through each file in the directory
-	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk(DATA_DIR, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-
-		// Only consider Markdown regular files (not directories)
 		if info.Mode().IsRegular() && filepath.Ext(path) == ".md" {
-			content, err := ioutil.ReadFile(path)
+			content, err := os.ReadFile(path)
 			if err != nil {
 				return err
 			}
@@ -101,7 +98,7 @@ func loadDocuments() ([]schema.Document, error) {
 	})
 
 	if err != nil {
-		return nil, fmt.Errorf("Error loading documents: %v\n", err)
+		return nil, fmt.Errorf("error loading documents: %v", err)
 	}
 
 	return documents, nil
